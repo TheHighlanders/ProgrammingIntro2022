e8ccd36982ce1c5bf7f8406779a5d873531353d0
Modular-Holmes <aneurin.hullah@gmail.com>
2022-10-11 16:44:16 -0400

    Added Drivetrain Code
    Code for Tank Drive added, and the slideshow is up-to-date

--- ProgrammingIntroProjects2022/src/main/java/frc/robot/Constants.java
+++ ProgrammingIntroProjects2022/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,9 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int kLeft1 = 1;
+    public static final int kLeft2 = 2;
+    public static final int kRight1 = 3;
+    public static final int kRight2 = 4;
+}
